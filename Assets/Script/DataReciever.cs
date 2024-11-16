b6f1781d4cf0102c8173caabf220378e14087938
ShaheerNazim <93708417+ShaheerNazim@users.noreply.github.com>
2024-11-16 05:27:58 +0500

    Google Analytics

--- Assets/Script/DataReciever.cs
+++ Assets/Script/DataReciever.cs
@@ -1,9 +1,54 @@
 using UnityEngine;
 using TMPro;
+using GameAnalyticsSDK;
 public class DataReceiver : MonoBehaviour
 {
     public TextMeshProUGUI Name;
+    public TextMeshProUGUI Error;
+    public void Start()
+    {
+        try
+        {
+            GameAnalyticsSDK.GameAnalytics.Initialize();
+        }
+        catch
+        {
+            //Error.text = "Some Issue With the GameAnalytics";
+        }
+        
+        //InvokeRepeating(nameof(SendEvent), 4f, 1f);
+    }
+    void SendEvent()
+    {
+        try
+        {
+            if (GameAnalytics.Initialized)
+            {
+                GameAnalyticsSDK.GameAnalytics.NewProgressionEvent(GameAnalyticsSDK.GAProgressionStatus.Start, "Bravo Entered The Space");
+                GameAnalyticsSDK.GameAnalytics.NewProgressionEvent(GameAnalyticsSDK.GAProgressionStatus.Start, "Shaheer Entered The Space");
+                Error.text = "Analytics Send";
 
+            }
+            else
+            {
+                try
+                {
+                    GameAnalytics.Initialize();
+
+                }
+                catch
+                {
+                    Error.text = "Some Issue With the GameAnalytics in the Invoke";
+                }
+                Error.text = "Analytics Not Initilized Send";
+
+            }
+        }
+        catch
+        {
+            Error.text = "Serious Issue With the Analytics";
+        }
+    }
     // This method will be called from JavaScript
     public void ReceiveData (string jsonData)
     {
@@ -11,8 +56,8 @@ public class DataReceiver : MonoBehaviour
         Data data = JsonUtility.FromJson<Data>(jsonData);
 
         // Now you can access the email and token
-        Name.text = data.displayName.ToUpper(); 
-
+        Name.text = data.displayName.ToUpper();
+        GameAnalyticsSDK.GameAnalytics.NewProgressionEvent(GameAnalyticsSDK.GAProgressionStatus.Start,data.displayName.ToUpper()+ " Entered The Space");
         // Handle the received data as needed
         // For example, you might want to store it or use it in your game
     }
